c21f23a6caecdc3bcb67860ec854409b685eadfa
Alex M <Alexm717@hotmail.com>
2022-08-06 21:08:19 -0700

    refactor naming

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,20 +1,20 @@
 require "./lib/alphabet"
 
 class Nightreader
-  attr_reader :read_file, :write_file
+  attr_reader :orginal_message_file, :encoded_message
 
-  def initialize(read_file, write_file)
-    @read_file_x = read_file
-    @write_file_x = write_file
+  def initialize(orginal_message_file, encoded_message)
+    @orginal_message_file = orginal_message_file
+    @encoded_message = encoded_message
     @translater = Alphabet.new.alphabet
   end
 
   def write_file_count
-    File.open(@read_file_x).size
+    File.open(@orginal_message_file).size
   end
 
   def read_file_and_translate
-    handle = File.open(@read_file_x, "r")
+    handle = File.open(@orginal_message_file, "r")
     incoming_text = handle.read.chomp
     handle.close
     translate(incoming_text)
@@ -36,7 +36,7 @@ class Nightreader
   end
 
   def write_file
-    writer = File.open(@write_file_x, "w")
+    writer = File.open(@encoded_message, "w")
     writer.write(read_file_and_translate)
     writer.close
   end
