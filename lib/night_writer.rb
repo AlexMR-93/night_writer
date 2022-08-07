d966fc78547359b47b5c2cdda45153864fe6ebba
Alex M <Alexm717@hotmail.com>
2022-08-06 21:37:10 -0700

    update file

--- lib/night_writer.rb
+++
@@ -1,4 +0,0 @@
-require_relative("./night_reader")
-
-night_reader = Nightreader.new(ARGV[0], ARGV[1])
-puts("Created #{ARGV[1]} containing #{night_reader.write_file_count} characters")
--- night_writer_runner.rb
+++ night_writer_runner.rb
@@ -1,5 +1,5 @@
-require_relative("./lib/night_reader")
+require_relative("./lib/english_braille")
 
-night_reader = Nightreader.new(ARGV[0], ARGV[1])
-night_reader.write_file
+night_reader = EnglishBraille.new(ARGV[0], ARGV[1])
+night_reader.e_file
 puts("Created #{ARGV[1]} containing #{night_reader.orginal_message_file_count} characters")
