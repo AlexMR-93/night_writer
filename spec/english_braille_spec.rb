f1950cc8586d5907426631bf7fe53bea6864ccfd
Alex M <Alexm717@hotmail.com>
2022-08-08 14:38:48 -0700

    test for english_braille

---
+++ spec/english_braille_spec.rb
@@ -0,0 +1,21 @@
+require "./lib/alphabet"
+require "./lib/english_braille"
+
+
+RSpec.describe(EnglishBraille) do
+  before((:each)) do
+    @english_braille = EnglishBraille.new("./spec/fixtures/test_input.txt", "./spec/fixtures/test_output.txt")
+  end
+
+  it("exists") do
+    expect(@english_braille).to(be_a(EnglishBraille))
+  end
+
+  it("can read text files") do
+    expect(@english_braille.file_reader).to(eq("alex"))
+  end
+
+  it("translate") do
+    expect(@english_braille.translate("alex")).to(eq("0.0.0.00\n..0..0..\n..0...00"))
+  end
+end
