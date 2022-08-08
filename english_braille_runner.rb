244abd3b6ae50fffe7d9ae5fdbcc0a76a9de2acc
Alex M <Alexm717@hotmail.com>
2022-08-08 14:25:28 -0700

    changed naming convention

--- english_braille_runner.rb
+++ english_braille_runner.rb
@@ -1,5 +1,5 @@
 require_relative("./lib/english_braille")
 
 night_reader = EnglishBraille.new(ARGV[0], ARGV[1])
-night_reader.e_file
+night_reader.encrypted_file
 puts("Created #{ARGV[1]} containing #{night_reader.orginal_message_file_count} characters")
