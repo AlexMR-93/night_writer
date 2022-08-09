44f881432bfd7aa53778ce7eae6ceddf02feb972
Alex M <Alexm717@hotmail.com>
2022-08-09 16:35:09 -0700

    iter.3 complete

--- english_braille_runner.rb
+++ english_braille_runner.rb
@@ -2,4 +2,4 @@ require_relative("./lib/english_braille")
 
 night_writer = EnglishBraille.new(ARGV[0], ARGV[1])
 night_writer.encrypted_file
-puts("Created #{ARGV[1]} containing #{night_reader.orginal_message_file_count} characters")
+puts("Created #{ARGV[1]} containing #{night_writer.orginal_message_file_count} characters")
